e81f9f0a04c0d380135f7a6831c36510842b79a5
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-10-07 02:31:31 -0700

    Just need to get the variables to display the results and plug that into the respectively formula somehow; no css done at all yet

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 import formulas from './data.js'
 
 const shapeDropdown = document.querySelector('#select-shape')
+const valueInputs = document.querySelector('#input-values')
+const footer = document.querySelector('footer')
+
 const defaultMeasurement = document.querySelector('#default-measurement')
 const defaultShape = document.querySelector('#default-shape')
 
@@ -13,6 +16,7 @@ const displayShapes = shapes => {
     shapeDropdown.innerHTML = selectShape
 }
 
+// Gets arguments and parameters needed to display inputs
 document.body.addEventListener('change', (e) => {
     if (e.target.matches('#select-measurement')) {
         const measurement = e.target.value
@@ -35,11 +39,16 @@ document.body.addEventListener('change', (e) => {
 })
 
 const displayInputs = vars => {
+    let inputValues = '<legend>Enter values:</legend>'
     for (let i = 0; i < vars.length; i++) {
-
+        inputValues += `<label for=${vars[i]}>${vars[i]}:
+            <input type="number" name="${vars[i]}">
+        </label>`
     }
+    inputValues += '<button id="input-submit">Calculate!</button>'
+    valueInputs.innerHTML = inputValues
 }
-
+// Gets arguments and parameters needed to display inputs
 document.body.addEventListener('change', (e) => {
     if (e.target.matches('#select-shape')) {
         const measurement = document.querySelector('#select-measurement').value
@@ -49,13 +58,44 @@ document.body.addEventListener('change', (e) => {
 
         console.log('measurement---------------------', measurement)
         console.log('vars---------------------', vars)
-        const formula = formulas[measurement][shape].formula
-        console.log('formula---------------------', formula)
+
+        displayInputs(vars)  
     }
     // Not working... yet
     // defaultShape.remove()
 })
 
+const displayResults = (values, formula) => {
+    // Need to plug in the corresponding variable to the formula
+    const result = ''
+
+
+    footer.innerHTML = `<p>Result:</p>`
+}
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.matches('#input-submit')) {
+        const measurement = document.querySelector('#select-measurement').value
+        const shape = document.querySelector('#select-shape').value
+        const vars = formulas[measurement][shape].vars
+        const formula = formulas[measurement][shape].formula
+
+        let values = {}
+        for (let i = 0; i < vars.length; i++) {
+            /* not exactly sure how the syntax of input[name="${vars[i]}"] works
+            (accidentally clicked the generate thing) but it still no work, I is struggling */
+            const input = document.querySelector(`input[name="${vars[i]}"]`)
+            values[vars[i]] = parseFloat(input)
+        }
+
+        console.log('formula---------------------', formula)
+        // values currently not working
+        console.log('values---------------------', values)
+        displayResults(values, formula)
+    }
+}) 
+        
+        
 
 
 
