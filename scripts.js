77e5b800a9edfb8700016aa2505a7d95a4c34f38
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-10-09 19:34:11 -0700

    Finished scripts (refer to last commit) and most CSS

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
 import formulas from './data.js'
 
+const measurementDropdown = document.querySelector('#select-measurement')
 const shapeDropdown = document.querySelector('#select-shape')
 const valueInputs = document.querySelector('#input-values')
-const footer = document.querySelector('footer')
-
-const defaultMeasurement = document.querySelector('#default-measurement')
-const defaultShape = document.querySelector('#default-shape')
+const showResult = document.querySelector('#show-result')
 
 const displayShapes = shapes => {
     // I dont know how to make this work with the default value being empty
-    let selectShape = '<option value="default-shape" id="default-shape"></option>'
+    let selectShape = '<option value="default-shape" name="default-shape" disabled selected>Select a shape</option>'
     for (let i = 0; i < shapes.length; i++) {
         selectShape += `<option value=${shapes[i]}>${shapes[i]}</option>`
     }
@@ -24,15 +22,18 @@ document.body.addEventListener('change', (e) => {
 
         if (measurement === 'area') {
             shapes = ['parallelogram', 'triangle', 'trapezoid', 'circle']
+            background = 
         } else if (measurement === 'circumference') {
             shapes = ['circle']
+            background = 
         } else if (measurement === 'volume') {
             shapes = ['circular-cylinder', 'sphere', 'circular-cone', 'pyramid']
+            background = 
         } else if (measurement === 'surfaceArea') {
             shapes = ['circular-cylinder', 'sphere']
+            background = 
         }
 
-        defaultMeasurement.remove()
         displayShapes(shapes)
         console.log('shapes---------------------', shapes)
     }
@@ -45,7 +46,7 @@ const displayInputs = vars => {
             <input type="number" name="${vars[i]}">
         </label>`
     }
-    inputValues += '<button id="input-submit">Calculate!</button>'
+    inputValues += '<button type="button" id="input-submit">Calculate!</button>'
     valueInputs.innerHTML = inputValues
 }
 // Gets arguments and parameters needed to display inputs
@@ -59,43 +60,47 @@ document.body.addEventListener('change', (e) => {
         console.log('measurement---------------------', measurement)
         console.log('vars---------------------', vars)
 
-        displayInputs(vars)  
+        displayInputs(vars)
+        valueInputs.hidden = false
     }
-    // Not working... yet
-    // defaultShape.remove()
 })
 
 const displayResults = (values, formula) => {
-    // Need to plug in the corresponding variable to the formula
-    const result = ''
+    // not to sure how it works but searched it up and gave me the below syntax
+    const result = formula(...Object.values(values)).toFixed(4)
 
-
-    footer.innerHTML = `<p>Result:</p>`
+    showResult.hidden = false
+    showResult.innerHTML = `<p id="result-desc">Result:</p>
+    <p id="result-value">${result}</p>`
 }
 
 document.body.addEventListener('click', (e) => {
     if (e.target.matches('#input-submit')) {
         const measurement = document.querySelector('#select-measurement').value
         const shape = document.querySelector('#select-shape').value
+
         const vars = formulas[measurement][shape].vars
         const formula = formulas[measurement][shape].formula
 
         let values = {}
         for (let i = 0; i < vars.length; i++) {
-            /* not exactly sure how the syntax of input[name="${vars[i]}"] works
-            (accidentally clicked the generate thing) but it still no work, I is struggling */
-            const input = document.querySelector(`input[name="${vars[i]}"]`)
-            values[vars[i]] = parseFloat(input)
+            const enteredValues = document.querySelector(`input[name="${vars[i]}"]`)
+            values[vars[i]] = parseFloat(enteredValues.value)
         }
 
         console.log('formula---------------------', formula)
-        // values currently not working
         console.log('values---------------------', values)
+
         displayResults(values, formula)
     }
 }) 
-        
-        
 
-
-
+// For refreshing page
+window.addEventListener('DOMContentLoaded', () => {
+    measurementDropdown.value = "default-measurement"
+    shapeDropdown.innerHTML = '<option value="refresh-shape" disabled selected> Select a measurement above </option>'
+    valueInputs.innerHTML = ''
+    valueInputs.hidden = true
+    showResult.innerHTML = ''
+    showResult.hidden = true
+})
